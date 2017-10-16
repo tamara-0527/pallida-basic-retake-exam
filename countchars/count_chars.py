34ba6013779cb57d3a2cee2b76178a5b396d65fc
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-16 12:18:32 +0200

    fix(count_chars): fix bugs

--- countchars/count_chars.py
+++ countchars/count_chars.py
@@ -13,35 +13,35 @@
 # 1 occured 3375 times in the file.
 # x occured 3232 times in the file.
 
-def reading_file():
-    with open("da_vinci_code.txt", "r") as used_file:
+import csv
+
+def reading_file(file_name):
+    with open('D:\greenfox\pallida-basic-retake-exam\countchars\da_vinci_code.txt', 'r') as used_file:
         used_file = used_file.read()
         return used_file
 
-
-def counted_letters(file_name):
+def counted_letters(used_file):
     letter_counter = {}
-    letters = reading_file()
+    letters = reading_file('da_vinci_code.txt')
     for letter in letters:
         if letter not in letter_counter:
-            if letter == 0:
+            if letter == "0":
                 letter_counter["0"] = 1
-            elif letter == 1:
+            elif letter == "1":
                 letter_counter["1"] = 1
-            elif letter == x:
+            elif letter == "x":
                 letter_counter["x"] = 1
         else:
-            if letter == 0:
+            if letter == "0":
                 letter_counter["0"] += 1
-            elif letter == 1:
+            elif letter == "1":
                 letter_counter["1"] += 1
-            elif letter == x:
+            elif letter == "x":
                 letter_counter["x"] += 1
     return letter_counter
 
 
-
 countedLetters = counted_letters("da_vinci_code.txt");
-print("0 occured " + countedLetters["0"] + " times in the file.");
-print("1 occured " + countedLetters["1"] + " times in the file.");
-print("x occured " + countedLetters["x"] + " times in the file.");
+print("0 occured " + str(countedLetters["0"]) + " times in the file.");
+print("1 occured " + str(countedLetters["1"]) + " times in the file.");
+print("x occured " + str(countedLetters["x"]) + " times in the file.");
