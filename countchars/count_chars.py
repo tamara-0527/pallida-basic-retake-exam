4f66f2ff61e662a7855d1ea771d14cb2320cf448
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-16 10:11:19 +0200

    feat(count_chars): add some function for counting

--- countchars/count_chars.py
+++ countchars/count_chars.py
@@ -12,3 +12,36 @@
 # 0 occured 3393 times in the file.
 # 1 occured 3375 times in the file.
 # x occured 3232 times in the file.
+
+def reading_file():
+    with open("da_vinci_code.txt", "r") as used_file:
+        used_file = used_file.read()
+        return used_file
+
+
+def counted_letters(file_name):
+    letter_counter = {}
+    letters = reading_file()
+    for letter in letters:
+        if letter not in letter_counter:
+            if letter == 0:
+                letter_counter["0"] = 1
+            elif letter == 1:
+                letter_counter["1"] = 1
+            elif letter == x:
+                letter_counter["x"] = 1
+        else:
+            if letter == 0:
+                letter_counter["0"] += 1
+            elif letter == 1:
+                letter_counter["1"] += 1
+            elif letter == x:
+                letter_counter["x"] += 1
+    return letter_counter
+
+
+
+countedLetters = counted_letters("da_vinci_code.txt");
+print("0 occured " + countedLetters["0"] + " times in the file.");
+print("1 occured " + countedLetters["1"] + " times in the file.");
+print("x occured " + countedLetters["x"] + " times in the file.");
